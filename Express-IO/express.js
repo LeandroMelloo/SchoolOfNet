f45a876a3053818a7417d23c69e85a9ad5dfe181
LeandroMelloo <leandromoreno1909@hotmail.com>
2019-01-20 15:04:12 -0200

    Alteração do codigo

--- Express-IO/express.js
+++ Express-IO/express.js
@@ -1,5 +1,27 @@
 var app = require('express.io')();
-app.http().io(); // http() -> servidor http, io() -> servidor WEbSocket.
+// var fs = require('fs');
+
+/* var httpsOptions = {
+    certificate: fs.readFileSync(__dirname.concat('certificate')),
+    key        : fs.readFileSync(__dirname.concat('key_certificate'))
+}*/
+
+// quando é colocado https, o websocket reconhece que é uma conexão segura.
+app.http().io(); // https() -> servidor https, io() -> servidor WEbSocket. //httpsOptions.
+
+/*app.io.route('init', function(req) {
+    req.io.emit('index:say_hello', {
+        message: 'Hello School of net Express.io'
+    });
+});*/
+
+app.io.route('init', function(req) {
+    console.log(req.data);
+    req.io.respond({
+        message: 'Hello School of net Express.io' + req.data.name,
+        number: req.data.number
+    });
+});
 
 app.get('/', function(req, res) {
    res.sendfile(__dirname.concat('/index.html')); 
